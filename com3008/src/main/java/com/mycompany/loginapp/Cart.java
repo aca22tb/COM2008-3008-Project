aea6926ff4ed82abed67f6ff422035c5273e769c
acb21cc <151275973+acb21cc@users.noreply.github.com>
2023-11-30 23:40:42 +0000

    Update Cart.java

--- com3008/src/main/java/com/mycompany/loginapp/Cart.java
+++ com3008/src/main/java/com/mycompany/loginapp/Cart.java
@@ -3,56 +3,63 @@ package com.mycompany.loginapp;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Map;
 
 public class Cart extends JFrame {
 
     private DefaultTableModel model;
     private JTable table;
-    private JButton returnButton, confirmOrderButton, clearCartButton;
+    private JButton deleteButton, confirmOrderButton, returnButton;
+    private Map<String, Integer> shoppingCart;
 
     public Cart(Map<String, Integer> shoppingCart) {
         setTitle("Shopping Cart");
         setSize(800, 400);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        // 创建表格和模型
-        model = new DefaultTableModel();
-        model.addColumn("Product Name");
-        model.addColumn("Quantity");
-        model.addColumn("Unit Price");
-        model.addColumn("Total Price");
-        model.addColumn("Remove");
+        this.shoppingCart = shoppingCart;
 
+        // 初始化表格模型
+        model = new DefaultTableModel();
+        model.addColumn("Brand");
+        model.addColumn("Product");
+        model.addColumn("Quantity");
+        model.addColumn("Price");
+        model.addColumn("Total Price");
+
+        // 初始化表格
         table = new JTable(model);
 
         // 创建滚动窗格
         JScrollPane scrollPane = new JScrollPane(table);
 
-        // 添加按钮监听器
-        returnButton = createStyledButton("Return");
+        // 初始化按钮
+        deleteButton = createStyledButton("Delete from Cart");
         confirmOrderButton = createStyledButton("Confirm Order");
-        clearCartButton = createStyledButton("Clear Cart");
+        returnButton = createStyledButton("Return to Shopping");
 
-        returnButton.addActionListener(e -> dispose()); // 关闭购物车界面，返回上一层
+        // 添加按钮监听器
+        deleteButton.addActionListener(e -> deleteFromCart());
         confirmOrderButton.addActionListener(e -> confirmOrder());
-        clearCartButton.addActionListener(e -> clearCart());
+        returnButton.addActionListener(e -> returnToShopping());
 
-        // 布局设置
+        // 创建底部面板
+        JPanel bottomPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        bottomPanel.add(deleteButton);
+        bottomPanel.add(confirmOrderButton);
+        bottomPanel.add(returnButton);
+
+        // 设置布局
         setLayout(new BorderLayout());
         add(scrollPane, BorderLayout.CENTER);
-
-        // 底部添加按钮的组件
-        JPanel bottomPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        bottomPanel.add(returnButton);
-        bottomPanel.add(confirmOrderButton);
-        bottomPanel.add(clearCartButton);
         add(bottomPanel, BorderLayout.SOUTH);
 
         // 显示购物车内容
-        displayShoppingCart(shoppingCart);
+        displayShoppingCart();
 
         setVisible(true);
     }
@@ -66,57 +73,103 @@ public class Cart extends JFrame {
     }
 
     // 显示购物车内容
-    private void displayShoppingCart(Map<String, Integer> shoppingCart) {
-        // 查询数据库或使用其他途径获取产品信息，此处仅为示例
+    private void displayShoppingCart() {
+        // 清空表格
+        model.setRowCount(0);
+
         for (Map.Entry<String, Integer> entry : shoppingCart.entrySet()) {
-            String productName = entry.getKey();
+            String productCode = entry.getKey();
             int quantity = entry.getValue();
-            double unitPrice = getProductPrice(productName);
-            double totalPrice = unitPrice * quantity;
 
-            Object[] rowData = {productName, quantity, unitPrice, totalPrice, "Remove"};
-            model.addRow(rowData);
+            try {
+                Connection connection = DatabaseConnection.getConnection();
+                String query = "SELECT brand, name, price FROM products WHERE code = ?";
+                try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+                    preparedStatement.setString(1, productCode);
+                    ResultSet resultSet = preparedStatement.executeQuery();
+
+                    if (resultSet.next()) {
+                        String brand = resultSet.getString("brand");
+                        String name = resultSet.getString("name");
+                        double price = resultSet.getDouble("price");
+
+                        Object[] rowData = {brand, name, quantity, price, quantity * price};
+                        model.addRow(rowData);
+                    }
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 
-    // 获取产品单价，此处为示例方法，实际项目中应该从数据库或其他途径获取
-    private double getProductPrice(String productName) {
-        // 假设产品单价如下
-        switch (productName) {
-            case "Product A":
-                return 20.0;
-            case "Product B":
-                return 15.0;
-            case "Product C":
-                return 30.0;
-            default:
-                return 0.0;
+    // 从购物车中删除产品
+    private void deleteFromCart() {
+        int selectedRow = table.getSelectedRow();
+
+        if (selectedRow != -1) {
+            // 获取选定行的产品信息
+            String product = (String) table.getValueAt(selectedRow, 1);
+
+            // 从购物车中移除该产品
+            shoppingCart.remove(product);
+
+            // 更新表格显示
+            model.removeRow(selectedRow);
+        } else {
+            JOptionPane.showMessageDialog(this, "Please select a product from the cart.");
         }
     }
 
-    // 确认下单
+    // 确认订单
     private void confirmOrder() {
-        // TODO: Add code to confirm the order and update the order table in the database
-        // 提示订单信息，此处为示例
-        JOptionPane.showMessageDialog(this, "Order confirmed!");
+        int choice = JOptionPane.showConfirmDialog(this, "Are you sure you want to confirm this order?", "Confirm Order", JOptionPane.YES_NO_OPTION);
+        if (choice == JOptionPane.YES_OPTION) {
+            // TODO: Add code to prompt the user for bank details and complete the order
+            showBankDetailsDialog();
+        }
     }
 
-    // 清空购物车
-    private void clearCart() {
-        int option = JOptionPane.showConfirmDialog(
-                this,
-                "Are you sure you want to clear the cart?",
-                "Clear Cart",
-                JOptionPane.YES_NO_OPTION,
-                JOptionPane.QUESTION_MESSAGE);
+    // 弹出对话框以输入银行信息
+    private void showBankDetailsDialog() {
+        JPanel panel = new JPanel(new GridLayout(6, 2));
 
-        if (option == JOptionPane.YES_OPTION) {
-            model.setRowCount(0); // 清空表格
-            // TODO: Add code to clear the shopping cart in the database
+        JTextField cardHolderNameField = new JTextField();
+        JTextField cardNumberField = new JTextField();
+        JTextField cvvField = new JTextField();
+        JTextField expiryDateField = new JTextField();
+
+        panel.add(new JLabel("Cardholder Name:"));
+        panel.add(cardHolderNameField);
+        panel.add(new JLabel("Card Number:"));
+        panel.add(cardNumberField);
+        panel.add(new JLabel("CVV:"));
+        panel.add(cvvField);
+        panel.add(new JLabel("Expiry Date:"));
+        panel.add(expiryDateField);
+
+        int result = JOptionPane.showConfirmDialog(this, panel, "Enter Bank Details", JOptionPane.OK_CANCEL_OPTION);
+
+        if (result == JOptionPane.OK_OPTION) {
+            // TODO: Add code to process the bank details and complete the order
+            JOptionPane.showMessageDialog(this, "Order confirmed! Bank details:\n" +
+                    "Cardholder Name: " + cardHolderNameField.getText() + "\n" +
+                    "Card Number: " + cardNumberField.getText() + "\n" +
+                    "CVV: " + cvvField.getText() + "\n" +
+                    "Expiry Date: " + expiryDateField.getText());
         }
+    }
+
+    // 返回购物界面
+    private void returnToShopping() {
+        dispose(); // 关闭当前界面
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new Cart(new java.util.HashMap<>()));
+        // For testing purposes
+        SwingUtilities.invokeLater(() -> {
+            Map<String, Integer> testCart = Map.of("Product A", 2, "Product B", 1, "Product C", 3);
+            new Cart(testCart);
+        });
     }
 }
