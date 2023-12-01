9da081a3070562ca022572a277804589665869eb
Tina Boumezoued <tboumezoued1@sheffield.ac.uk>
2023-12-01 02:53:04 +0000

    making the font smaller

--- com3008/src/main/java/com/mycompany/loginapp/Customer.java
+++ com3008/src/main/java/com/mycompany/loginapp/Customer.java
@@ -11,7 +11,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.HashMap;
 import java.util.Map;
-///testing for jeannie
+
 public class Customer extends JFrame {
 
     private JButton searchButton, viewCartButton, viewOrderHistoryButton, editProfileButton, logoutButton;
@@ -112,8 +112,8 @@ public class Customer extends JFrame {
     // 创建样式化按钮
     private JButton createStyledButton(String buttonText) {
         JButton button = new JButton(buttonText);
-        button.setPreferredSize(new Dimension(200, 40));
-        button.setFont(new Font("Arial", Font.PLAIN, 16));
+        button.setPreferredSize(new Dimension(200, 24));
+        button.setFont(new Font("Arial", Font.PLAIN, 12));
         return button;
     }
 
