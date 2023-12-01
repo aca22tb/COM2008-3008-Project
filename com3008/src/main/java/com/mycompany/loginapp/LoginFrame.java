d08a0071b0e4d8483f6b7ab36c6a721bba88c3c9
Kamonlak Tan <ktan11@sheffield.ac.uk>
2023-12-01 00:36:05 +0000

    made the button look nicer

--- com3008/src/main/java/com/mycompany/loginapp/LoginFrame.java
+++ com3008/src/main/java/com/mycompany/loginapp/LoginFrame.java
@@ -1,7 +1,7 @@
 package com.mycompany.loginapp;
 
 import javax.swing.*;
-import java.awt.*;
+// import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
