dc81970accd55f6e734a605c7e58f5fa658ef1ca
Tina Boumezoued <tboumezoued1@sheffield.ac.uk>
2023-12-01 02:46:28 +0000

    jeannie

--- com3008/src/main/java/com/mycompany/loginapp/Customer.java
+++ com3008/src/main/java/com/mycompany/loginapp/Customer.java
@@ -2,10 +2,10 @@ package com.mycompany.loginapp;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableColumn;
+// import javax.swing.table.TableColumn;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+// import java.awt.event.ActionEvent;
+// import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
